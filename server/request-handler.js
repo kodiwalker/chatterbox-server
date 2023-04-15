93b04e5e11cf53185ec3cdcf7cae91cb3589a480
Kodi <kodi.w13@gmail.com>
2023-04-14 18:37:20 -0600

    Implemented and tested GET and POST requests

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,10 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
+var requestHandler = function (request, response) {
+  let messages = [{ name: 'abc' }];
+  // const { headers, method, url } = request;
+
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -23,27 +26,62 @@ var requestHandler = function(request, response) {
   // http://nodejs.org/documentation/api/
 
   // Do some basic logging.
-  //
+
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  if (request.url !== '/classes/messages') {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write('404 Not found');
+    response.end();
+  } else {
 
+    let statusCode;
+    console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+    var headers = defaultCorsHeaders;
+
+
+    if (request.method === 'POST') {
+      statusCode = 201;
+      let body = [];
+      request.on('data', (chunk) => {
+
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        messages.push(JSON.parse(body));
+
+        headers['Content-Type'] = 'application/json';
+        headers['Data-Type'] = 'application/json';
+        response.writeHead(statusCode, headers);
+
+        response.end(body);
+
+      });
+
+
+    } else if (request.method === 'GET') {
+      statusCode = 200;
+      headers['Content-Type'] = 'application/json';
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(messages));
+    }
+  }
   // The outgoing status.
-  var statusCode = 200;
 
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  //^ headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+
+  //^ response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +90,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -70,3 +107,6 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept, authorization',
   'access-control-max-age': 10 // Seconds.
 };
+
+
+exports.requestHandler = requestHandler;
